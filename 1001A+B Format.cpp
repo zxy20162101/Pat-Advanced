5a7fd67aaa442bfad192e11e7503148a17dd0231
zhangxiaoyu123456 <384922529@qq.com>
2019-07-22 01:31:11 +0800

    modify rightly

--- 1001A+B Format.cpp
+++ 1001A+B Format.cpp
@@ -1,20 +1,19 @@
 #include<iostream>
-#include<string>
 using namespace std;
-
 int main(){
     int a,b;
     cin>>a>>b;
     int c=a+b;
     string out = to_string(c);
-    int cnt=0;
-    if(out[0]!='-'){
-        cnt++;
-    }
-    else   cout<<'-';
-    for(int i=1;i<out.length();i++){
-        if(cnt%3==0 && i!=out.length()-1 && i!=1)cout<<',';
+    for(int i=0;i<out.length();i++){
         cout<<out[i];
-        cnt++;
+        if(out[i]=='-')continue;
+        if((i+1)%3==out.length()%3 && i!=out.length()-1 ){
+			cout<<',';
+		} 
     }
 }
+
+//4,555,555
+//咱也不知道这个规律是怎么看出来的，好笨
+//注意是从右往左 
